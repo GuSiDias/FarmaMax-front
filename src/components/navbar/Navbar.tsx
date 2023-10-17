54325edf57345f398a11bdfdda9f0c50cff0f16a
gusta <gusidias@gmail.com>
2023-10-17 17:50:10 -0300

    navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -7,14 +7,14 @@ import car from '../../assets/carrinho.png'
 
 function Navbar() {
     return (
-        <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
-          <div className="container flex items-center justify-between text-lg">
-            <div className='max-w-xs max-h-20'> <img src={logo} alt='' className=''/> </div>
+        <div className='w-full bg-indigo-900 text-white flex justify-center py-3'>
+          <div className="container flex items-center justify-between text-lg me-8 ">
+            <div className=''> <img src={logo} alt='' className='w-3/4 '/> </div>
 
-            <div className='flex'>
-            <label className="relative block w-96">
-                  <input className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Pesquisar" type="text" name="search"/>
-                  </label>
+            <div className='flex px-2.5'>
+              <label className="relative block w-96">
+                <input className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 text-black rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Pesquisar" type="text" name="search"/>
+              </label>
             </div>
 
             <div className='flex  gap-4'>
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import homeLogo from '../../assets/home.png'
 import './Home.css';
+import ListarProduto from '../../components/produto/listaProduto/ListaProduto';
+
 
 function Home() {
     return (
@@ -8,12 +10,12 @@ function Home() {
         <div className="bg-cyan-300 flex justify-center">
           <div className='container grid grid-cols-2 text-white'>
             <div className="flex flex-col gap-4 items-center justify-center py-4">
-              <h2 className='text-5xl font-bold'>Seja bem vinde!</h2>
-              <p className='text-xl'>Seus medicamentos e cosméticos você so encontra aqui!</p>
+              <h2 className='text-5xl text-black font-bold'>Seja bem vinde!</h2>
+              <p className='text-xl text-black'>Seus medicamentos e cosméticos você so encontra aqui!</p>
   
               <div className="flex justify-around gap-4">
               
-                <button className='rounded bg-white text-blue-800 py-2 px-4'>Cadastrar produtos</button>
+                <button className='rounded bg-indigo-900 text-white py-2 px-4'>Cadastrar produtos</button>
               </div>
             </div>
   
@@ -23,6 +25,7 @@ function Home() {
             </div>
           </div>
         </div>
+        <div className='bg-red-100'><ListarProduto/></div>
         
       </>
     );
